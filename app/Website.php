6606b490e9f13337d3da2b1af37780bc44682aa3
Mauricio Rodrigues <mmauricio.vsr@gmail.com>
2018-11-07 16:52:56 -0300

    create website model and factory

--- app/Website.php
+++ app/Website.php
@@ -18,4 +18,20 @@ class Website extends Model
         'created_at',
         'updated_at'
     ];
+
+    /**
+     * @var array
+     */
+    public static $arrActive = [
+        'Y' => 'Yes',
+        'N' => 'No',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
--- database/migrations/2018_11_07_192530_create_websites_table.php
+++ database/migrations/2018_11_07_192530_create_websites_table.php
@@ -15,7 +15,13 @@ class CreateWebsitesTable extends Migration
     {
         Schema::create('websites', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->enum('active', ['Y', 'N'])->default('n');
+            $table->string('name');
+            $table->string('domain')->index();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('SET NULL');
         });
     }
 
